ef2f999ce51f1b26a879005f7f827a09c2e0b483
Nurfaizzi <113836263+Nurfaizzi@users.noreply.github.com>
2022-10-09 17:07:08 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+
+       function faktorial(k){
+        var angka=1;
+        var faktor=1;
+         while(angka<=k){
+             faktor=faktor*angka;
+             angka=angka+1;
+         }
+         console.log(faktor)
+    }
+    faktorial(7)
